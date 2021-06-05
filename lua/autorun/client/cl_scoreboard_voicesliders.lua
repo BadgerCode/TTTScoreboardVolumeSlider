1d02fffabf27eb1d17e63f774ca7f1a12671ecbb
Michael Hawkins <BadgerCode@users.noreply.github.com>
2021-06-05 12:40:45 +0100

    Jazz up styles massively

--- lua/autorun/client/cl_scoreboard_voicesliders.lua
+++ lua/autorun/client/cl_scoreboard_voicesliders.lua
@@ -19,7 +19,7 @@ local function RenderVoiceSlider(ply)
     end
 
     -- TODO: Keep this timer?
-    timer.Simple(20, function()
+    timer.Simple(10, function()
         if IsValid(frame) then
             frame:Close()
         end
@@ -29,27 +29,25 @@ local function RenderVoiceSlider(ply)
     currentValue = currentValue != nil and currentValue or 1
 
 
-
-    local text = vgui.Create("DLabel", frame)
-    text:SetPos(padding, padding)
-    text:SetSize(width - padding * 2, 20)
-	text:SetColor(Color(255, 255, 255, 255))
-    text.UpdateValue = function(self, newValue)
-        self:SetText(math.Round(newValue * 100) .. "%")
-    end
-    text:UpdateValue(currentValue)
-    text:SetContentAlignment(5)
+    local label = vgui.Create("DLabel", frame)
+    label:SetPos(padding, padding)
+    label:SetFont("cool_small")
+    label:SetSize(width - padding * 2, 20)
+	label:SetColor(Color(255, 255, 255, 255))
+    label:SetText("Player Volume")
 
 
+    local sliderHeight = 16
+    local sliderDisplayHeight = 8
 
     local slider = vgui.Create( "DSlider", frame)
-	slider:SetHeight(16)
+	slider:SetHeight(sliderHeight)
     slider:Dock(TOP)
+    slider:DockMargin(padding, 0, padding, 0)
     slider:SetSlideX(currentValue)
 	slider:SetLockY(0.5)
 	slider.TranslateValues = function(slider, x, y)
         ply:SetVoiceVolumeScale(x)
-        text:UpdateValue(x)
         return x, y
     end
 
@@ -63,23 +61,29 @@ local function RenderVoiceSlider(ply)
 		frame:Close()
 	end
 
-    Derma_Hook(slider, "Paint", "Paint", "NumSlider")
+    slider.Paint = function(self, w, h)
+        draw.RoundedBox(5, 0, sliderDisplayHeight / 2, w, sliderDisplayHeight, Color(200, 46, 46, 255))
+    end
 
+    slider.Knob.Paint = function(self, w, h)
+        if(slider:IsEditing()) then
+            local textValue = math.Round(slider:GetSlideX() * 100) .. "%"
+            local textPadding = 5
 
-    -- local DermaNumSlider = vgui.Create( "DNumSlider", DermaPanel )
-    -- DermaNumSlider:SetPos( 0, 0 )				-- Set the position
-    -- DermaNumSlider:SetSize( 300, 100 )			-- Set the size
-    -- DermaNumSlider:SetMin( 0 )				 	-- Set the minimum number you can slide to
-    -- DermaNumSlider:SetMax( 100 )				-- Set the maximum number you can slide to
-    -- DermaNumSlider:SetDecimals( 0 )				-- Decimal places - zero for whole number
-    -- DermaNumSlider:SetValue(ply:GetVoiceVolumeScale() * 100)
-
-    -- -- If not using convars, you can use this hook + Panel.SetValue()
-    -- DermaNumSlider.OnValueChanged = function( self, value )
-    --     local newVolumeScale = math.Round(value / 100, 3)
-    --     print("Value changed to " .. newVolumeScale .. " for " .. ply:Nick())
-    --     ply:SetVoiceVolumeScale(newVolumeScale)
-    -- end
+            -- The position of the text and size of rounded box are not relative to the text size. May cause problems if font size changes
+            draw.RoundedBox(
+                5, -- Radius
+                -sliderHeight * 0.5 - textPadding, -- X
+                -25, -- Y
+                sliderHeight * 2 + textPadding * 2, -- Width
+                sliderHeight + textPadding * 2, -- Height
+                Color(52, 54, 57, 255)
+            )
+            draw.DrawText(textValue, "cool_small", sliderHeight / 2, -20, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER)
+        end
+        
+        draw.RoundedBox(100, 0, 0, sliderHeight, sliderHeight, Color(255, 255, 255, 255))
+    end
 end
 
 -- TODO: remove, or turn into a proper command with a player target
@@ -92,94 +96,22 @@ end)
 
 
 
-print("Reloading volume slider")
+print("Reloading volume slider") -- TODO: Delete
 
 hook.Remove("TTTScoreboardColumns", "TTTScoreboardMenuVoiceSliders")
 hook.Add("TTTScoreboardColumns","TTTScoreboardMenuVoiceSliders", function(pnl)
-    print("Scoreboard initialised") -- TODO: Remove
-
-    -- TODO: Remove
-    /*
-    self.voice = vgui.Create("DImageButton", self)
-    self.voice:SetSize(16,16)
-
-    if self.Player != LocalPlayer() then
-        local muted = self.Player:IsMuted()
-        self.voice:SetImage(muted and "icon16/sound_mute.png" or "icon16/sound.png")
-    else
-        self.voice:Hide()
-    end
-
-    self.voice:SetVisible(not self.open)
-    self.voice:SetSize(16, 16)
-    self.voice:DockMargin(4, 4, 4, 4)
-    self.voice:Dock(RIGHT)
-
-    self.voice.DoClick = function()
-        if IsValid(ply) and ply != LocalPlayer() then
-            ply:SetMuted(not ply:IsMuted())
-        end
-    end
-
-    https://wiki.facepunch.com/gmod/DNumSlider
-
-
-
-    pnl:AddColumn("ID", function(ply) return ply:UserID() end)
-    pnl:GetPlayer()
-
-    1. Wait 1 tick and add a volume slider column
-    2. Override paint
-    3. Override DoClick/RightClick
-    4. Remove other volumne icon?
-
-    */
-
     
     -- Skip the header row
     if pnl.GetPlayer == nil then return end
 
-    -- This timer allows the scoreboard to add the mute button, which happens after this function runs
+    -- Wait for the scoreboard to finish adding the mute (voice) button, before trying to modify it
+    -- We have to wait for the panel's player to be set too
     timer.Simple(1, function()
-        local rowPlayer = pnl:GetPlayer()
-
-        if (rowPlayer == LocalPlayer()) then return end
-        print("Adding custom voice button for " .. rowPlayer:Nick()) -- TODO: Remove
-
         pnl.voice.DoRightClick = function()
-            print("Right clicked on custom voice for " .. rowPlayer:Nick()) -- TODO: Remove
+            local rowPlayer = pnl:GetPlayer()
+            if (rowPlayer == LocalPlayer() or not IsValid(rowPlayer)) then return end
 
             RenderVoiceSlider(rowPlayer)
         end
-
-        -- TODO: Remove
-        -- for _, v in ipairs( pnl:GetChildren() ) do
-        --     print( v:GetClassName() )
-        -- end
-
-        -- local voiceSlider = vgui.Create("DImageButton", pnl)
-        -- voiceSlider:SetSize(16,16)
-        -- voiceSlider.Think = function(self)
-        --     local muted = rowPlayer:IsMuted()
-        --     voiceSlider:SetImage(muted and "icon16/sound_mute.png" or "icon16/sound.png")
-        -- end
-        -- voiceSlider.DoClick = function()
-        --     print("Clicked on custom voice")
-        --     if IsValid(rowPlayer) then
-        --         rowPlayer:SetMuted(not rowPlayer:IsMuted())
-        --     end
-        -- end
-
-        -- voiceSlider.DoRightClick = function()
-        --     print("Right clicked on custom voice")
-        --     if not IsValid(rowPlayer) then return end
-
-        --     -- TODO: 
-        -- end
-
-        -- voiceSlider:SetVisible(true)
-        -- voiceSlider:SetSize(16, 16)
-        -- voiceSlider:DockMargin(4, 4, 4, 4)
-        -- voiceSlider:Dock(RIGHT)
     end)
 end)
